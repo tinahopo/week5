53963fec64752c98eaa15bc784c04416a4a56aba
雷古娜 <tinahopo@gmail.com>
2021-08-05 22:41:38 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,27 @@
+$(function () {
+  console.log('Hello Bootstrap5');
+});
+
+
+
+$(document).ready(function () {
+  $('.expand_less').hide();
+  $('.expand_more').show();
+  $('.expand').click(function (e) {
+    e.preventDefault();
+    $('.expand_less').show();
+    $('.expand_more').hide();
+  if ('aria-expanded=="true"') {
+    $('.expand_less').show();
+  } else {
+    $('.expand_less').hide();
+  }
+  // if ('aria-expanded="false"') {
+  //   $('.expand_more').show();
+  // } else {
+  //   $('.expand_more').hide();
+  // }
+  })
+  $('.expand_less').toggleClass(active);
+  
+});
